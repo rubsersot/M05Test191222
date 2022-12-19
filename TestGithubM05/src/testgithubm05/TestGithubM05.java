5c8af3c6bfc752b32bec389544404e7480a4ebb7
ausias <ausias@hINFO6-24.xiam.cat>
2022-12-19 11:42:38 +0100

    prueba

---
+++ TestGithubM05/src/testgithubm05/TestGithubM05.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testgithubm05;
+
+/**
+ *
+ * @author ausias
+ */
+public class TestGithubM05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Primera prueba
+    }
+    
+}
