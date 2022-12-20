ef881cf093cc93ff67ea863504d82e52974071c0
ruben <>
2022-12-20 13:16:34 +0100

    Inici Practica 8

---
+++ UF2_Practica8-Pablo_Jimenez-Ruben_Serrano/src/uf2_practica8/pablo_jimenez/ruben_serrano/UF2_Practica8Pablo_JimenezRuben_Serrano.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uf2_practica8.pablo_jimenez.ruben_serrano;
+
+/**
+ *
+ * @author ausias
+ */
+public class UF2_Practica8Pablo_JimenezRuben_Serrano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
