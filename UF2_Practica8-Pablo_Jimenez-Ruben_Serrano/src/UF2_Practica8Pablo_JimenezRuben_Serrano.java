ca26b5ecfcaf7bcb834736402853a1c211c68578
Pablo Jiménez <Pablo Jimnez@DESKTOP-UEAH07A>
2023-01-03 17:27:07 +0100

    Actualitzacio 2

--- UF2_Practica8-Pablo_Jimenez-Ruben_Serrano/src/UF2_Practica8Pablo_JimenezRuben_Serrano.java
+++ UF2_Practica8-Pablo_Jimenez-Ruben_Serrano/src/UF2_Practica8Pablo_JimenezRuben_Serrano.java
@@ -6,11 +6,56 @@ public class UF2_Practica8Pablo_JimenezRuben_Serrano {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        int moviment = 0;
+
         //Demanem la mida del tauler en files i columnes       
         int fila = escanearEntero("Quantes files vols: ");
         int columna = escanearEntero("Quantes columnes vols: ");
-        int [][] tauler = new int [fila][columna];
+        int[][] tauler = new int[fila][columna];
+
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                tauler[i][j] = 0;
+            }
+        }
+        int jugador = 1;
+        boolean ratlla = false;
+
+        while (ratlla == false) {
+            boolean jugada;
+            do {
+                Dibuixa(tauler);
+                moviment = escanearEntero("Selecciona una fila: ");
+                
+                //Comprobem la jugada
+                jugada = Jugada(moviment, tauler);
+            } while (jugada == false);
+            //Coloquem la fitxa
+            for (int f = tauler.length - 1; f >= 0; f--) {
+                if (tauler[f][moviment] == 0) {
+                    tauler[f][moviment] = jugador;
+                   
+                }
+            }
+            //Comprobem si hi ha guanyador
+            ratlla = EnRatlla(jugador, tauler);
+
+            //Si no intercambiem els jugadors
+            if (jugador == 1) {
+                jugador = 2;
+            } else {
+                jugador = 1;
+            }
+            Dibuixa(tauler);
+
+            if (ratlla) {
+                if (jugador == 1) {
+                    System.out.println("GUANYA EL JUGADOR 1!!");
+                } else {
+                    System.out.println("GUANYA EL JUGADOR 2 !!");
+                }
+            }
+        }
 
     }
 
@@ -26,19 +71,55 @@ public class UF2_Practica8Pablo_JimenezRuben_Serrano {
         return num;
     }
 
-    static int Jugada(int [][] tauler){
-    
+    static boolean Jugada(int columna, int tauler[][]) {
+        boolean jugada = true;
+        //Primer comprobem si la jugada es valida
+        if (columna < 0 || columna > tauler.length) {
+            jugada = false;
+        }
+        //Comprobem si està plena 
+        if (tauler[0][columna] != 0) {
+            jugada = false;
+        }
+
+        return jugada;
     }
-    
-    static int EnRatlla(int [][] tauler){
-        
+
+    static boolean EnRatlla(int jugador, int tauler[][]) {
+        boolean ratlla = false;
+        //Comprobem en vertical
+        for (int fila = 0; fila < tauler.length - 3; fila++) {
+            for (int col = 0; col < tauler[0].length; col++) {
+                if (tauler[fila][col] == jugador
+                        && tauler[fila+1][col] == jugador
+                        && tauler[fila+2][col] == jugador
+                        && tauler[fila+3][col] == jugador) {
+                    ratlla = true;
+                }
+            }
+        }
+        //Comprobem en hortizontal
+        for (int fila = 0; fila < tauler.length; fila++) {
+            for (int col = 0; col < tauler[0].length - 3; col++) {
+                if (tauler[fila][col] == jugador
+                        && tauler[fila][col+1] == jugador
+                        && tauler[fila][col+2] == jugador
+                        && tauler[fila][col+3] == jugador) {
+                    ratlla = true;
+                }
+            }
+        }
+        return ratlla;
     }
-    
-    static void Dibuixa(int fila, int columna, int [][] tauler){
-      
+
+    static void Dibuixa(int tauler[][]) {
+
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler.length; j++) {
+                System.out.print(tauler[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
     }
-    
-    
-    
 
 }
